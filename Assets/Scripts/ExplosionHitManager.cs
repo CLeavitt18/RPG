665cd0a263a1014c69a21d3ccd81062d57c352e0
Collin Leavitt <zanthor0128@gmail.com>
2022-11-29 17:21:39 -0500

    Add damage multiplier based on how far away the thing is from the explosion after a charged spell hits something.

--- Assets/Scripts/ExplosionHitManager.cs
+++ Assets/Scripts/ExplosionHitManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosionHitManager : HitManager
@@ -13,6 +11,17 @@ public class ExplosionHitManager : HitManager
     {
         if (other.GetComponent<LivingEntities>() != null)
         {
+            float distance = Vector3.Distance(transform.position, other.transform.position) + 0.2f;
+
+            float range = GetComponent<Light>().range;
+
+            float damageMulti = -0.5f * ((distance / range) - 1.0f);
+
+            for (int i = 0; i < Stats.DamageValues.Count; i++)
+            {
+                Stats.DamageValues[i] = (int)((float)Stats.DamageValues[i] * damageMulti);
+            }
+
             HitSomething(other.GetComponent<LivingEntities>(), false);
         }    
     }
